ac3b78964bd6ead90c953b524745fe259e8bf321
kimhyunso <gustn9515@gmail.com>
2024-01-31 23:28:22 +0900

    20240131 23:28

--- demo/src/main/java/com/jpa/demo/Main.java
+++ demo/src/main/java/com/jpa/demo/Main.java
@@ -1,6 +1,8 @@
 package com.jpa.demo;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
@@ -8,15 +10,15 @@ public class Main {
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
 
 
-//    public static void main(String[] args) {
-//
-//        EntityManager em = emf.createEntityManager();
-//        EntityTransaction tx = em.getTransaction();
-//
-//        tx.begin();
-//
-//        tx.commit();
-//    }
+    public static void main(String[] args) {
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        tx.commit();
+    }
 
 
 }
