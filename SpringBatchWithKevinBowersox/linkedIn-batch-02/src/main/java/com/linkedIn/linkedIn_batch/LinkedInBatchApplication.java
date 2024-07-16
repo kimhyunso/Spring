fb35dc31b55214d38fb5178b93369e2a6c616642
kimhyunso <gustn9515@gmail.com>
2024-07-16 18:00:38 +0900

    add Batch 추가

---
+++ SpringBatchWithKevinBowersox/linkedIn-batch-02/src/main/java/com/linkedIn/linkedIn_batch/LinkedInBatchApplication.java
@@ -0,0 +1,13 @@
+package com.linkedIn.linkedIn_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInBatchApplication.class, args);
+	}
+
+}
