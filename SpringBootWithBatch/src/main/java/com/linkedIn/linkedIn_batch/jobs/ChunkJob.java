a2c74447cd0c6df3a49980b9b41e31348df2173c
kimhyunso <gustn9515@gmail.com>
2024-08-05 00:29:53 +0900

    fix: JDBCitemWriter추가

--- SpringBootWithBatch/src/main/java/com/linkedIn/linkedIn_batch/jobs/ChunkJob.java
+++ SpringBootWithBatch/src/main/java/com/linkedIn/linkedIn_batch/jobs/ChunkJob.java
@@ -1,6 +1,7 @@
 package com.linkedIn.linkedIn_batch.jobs;
 
 import com.linkedIn.linkedIn_batch.domain.Orders;
+import com.linkedIn.linkedIn_batch.jobs.writer.OrderItemPreparedStatementSetter;
 import com.linkedIn.linkedIn_batch.mapper.OderRowMapper;
 import com.linkedIn.linkedIn_batch.mapper.OrderFieldSetMapper;
 import lombok.RequiredArgsConstructor;
@@ -11,16 +12,19 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,9 +38,12 @@ import java.util.Map;
 @Configuration
 @RequiredArgsConstructor
 public class ChunkJob {
+    private static String INSERT_ORDER_SQL = "insert into SHIPPED_ORDER_OUTPUT(order_id, first_name, last_name, email, cost, item_id, item_name, ship_date) "
+            + "values(?, ?, ?, ?, ?, ?, ?, ?)";
     private final SqlSessionFactory sqlSessionFactory;
     private final DataSource dataSource;
     private static String[] tokens = new String[] {"order_id", "first_name", "last_name", "email", "cost", "item_id", "item_name", "ship_date"};
+    private static String[] names = new String[] {"orderId", "firstName", "lastName", "email", "cost", "itemId", "itemName", "shipDate"};
 
     private static String ORDER_SQL = "select order_id, first_name, last_name, "
             + "email, cost, item_id, item_name, ship_date "
@@ -111,21 +118,39 @@ public class ChunkJob {
                 .pageSize(10)
                 .build();
     }
+    @Bean
+    public ItemWriter<Orders> itemWriter() {
+        FlatFileItemWriter<Orders> itemWriter = new FlatFileItemWriter<>();
+        itemWriter.setResource(new FileSystemResource("/data/shipped_orders_output.csv"));
+
+        DelimitedLineAggregator<Orders> aggregator = new DelimitedLineAggregator<>();
+        aggregator.setDelimiter(",");
+
+        BeanWrapperFieldExtractor<Orders> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(names);
+
+        aggregator.setFieldExtractor(fieldExtractor);
+        itemWriter.setLineAggregator(aggregator);
+
+        return itemWriter;
+    }
+    @Bean
+    public ItemWriter<Orders> jdbcBatchItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Orders>()
+                .dataSource(dataSource)
+                .sql(INSERT_ORDER_SQL)
+                .itemPreparedStatementSetter(new OrderItemPreparedStatementSetter())
+                .build();
+    }
 
     @Bean
     public Step chunkBasedStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
         return new StepBuilder("chunkBasedStep", jobRepository)
                 .<Orders, Orders>chunk(10, transactionManager)
-                .reader(myBatisCursorItemReader())
-                .writer(new ItemWriter<Orders>() {
-                    @Override
-                    public void write(Chunk<? extends Orders> chunk) throws Exception {
-                        System.out.println(String.format("Received List of Size: %s", chunk.size()));
-                        chunk.forEach(System.out::println);
-                        // file 만들기 로직
-                    }
-                }).build();
+                .reader(jbdcPaingItemReader())
+                .writer(jdbcBatchItemWriter()).build();
     }
+
 
     @Bean
     public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
---
+++ SpringBootWithBatch/src/main/java/com/linkedIn/linkedIn_batch/jobs/writer/OrderItemPreparedStatementSetter.java
@@ -0,0 +1,23 @@
+package com.linkedIn.linkedIn_batch.jobs.writer;
+
+import com.linkedIn.linkedIn_batch.domain.Orders;
+import org.springframework.batch.item.database.ItemPreparedStatementSetter;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class OrderItemPreparedStatementSetter implements ItemPreparedStatementSetter<Orders> {
+
+    @Override
+    public void setValues(Orders item, PreparedStatement ps) throws SQLException {
+        ps.setLong(1, item.getOrderId());
+        ps.setString(2, item.getFirstName());
+        ps.setString(3, item.getLastName());
+        ps.setString(4, item.getEmail());
+        ps.setBigDecimal(5, item.getCost());
+        ps.setString(6, item.getItemId());
+        ps.setString(7, item.getItemName());
+        ps.setDate(8, new Date(item.getShipDate().getTime()));
+    }
+}
