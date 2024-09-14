fad3f966b224ec0d3f52dc43f69d96fda30c0ef9
kimhyunso <gustn9515@gmail.com>
2024-09-14 23:06:35 +0900

    feat: springcloud requestHeader responseHeader 추가

--- SpringBootWithSpringCloud/first-service/src/main/java/com/example/first_service/WelcomeController.java
+++ SpringBootWithSpringCloud/first-service/src/main/java/com/example/first_service/WelcomeController.java
@@ -1,14 +1,23 @@
 package com.example.first_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class WelcomeController {
     @GetMapping("/welcome")
     public String welcome() {
         return "welcome first service application";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "hello header first service " + header;
     }
 }
--- SpringBootWithSpringCloud/second-service/src/main/java/com/example/second_service/WelcomeController.java
+++ SpringBootWithSpringCloud/second-service/src/main/java/com/example/second_service/WelcomeController.java
@@ -1,9 +1,12 @@
 package com.example.second_service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class WelcomeController {
@@ -11,4 +14,12 @@ public class WelcomeController {
     public String welcome() {
         return "welcome second service application";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "hello header second service " + header;
+    }
+
+
 }
