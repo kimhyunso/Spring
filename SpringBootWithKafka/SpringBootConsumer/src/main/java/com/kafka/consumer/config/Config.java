fb9319139acff2245bcb9c36081e2e271a1e7e1b
kimhyunso <gustn9515@gmail.com>
2024-12-02 13:34:37 +0900

    fix: kafka consumer Config 수정

--- SpringBootWithKafka/SpringBootConsumer/src/main/java/com/kafka/consumer/config/Config.java
+++ SpringBootWithKafka/SpringBootConsumer/src/main/java/com/kafka/consumer/config/Config.java
@@ -2,6 +2,7 @@ package com.kafka.consumer.config;
 
 import com.kafka.consumer.listener.FooListener;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,7 +37,7 @@ public class Config {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
