f21bcbaacf097591670ba36e7e8d0ec6d62e53dc
kimhyunso <gustn9515@gmail.com>
2025-02-04 14:19:51 +0900

    fix: globalExceptionHandler 클래스 변경

--- SpringBootWithAOP/src/main/java/com/example/aop/exception/GlobalExceptionHandler.java
+++ SpringBootWithAOP/src/main/java/com/example/aop/exception/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package com.example.aop.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
